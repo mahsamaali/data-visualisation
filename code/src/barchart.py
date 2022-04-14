1daf2ebe81ecfec0aade869455d5c3260c4ff5c5
mahsamaali <52839763+mahsamaali@users.noreply.github.com>
2022-04-13 22:28:09 -0400

    Add files via upload

---
+++ code/src/barchart.py
@@ -0,0 +1,86 @@
+import plotly.express as px
+
+def barchart_gratuit(df):
+
+
+	fig = px.bar(df, x='groupe', y=["événements_gratuits", "événement_payant"] )
+
+	fig.update_layout(
+		title="Barchart pour les événements gratuits et payants.",
+		xaxis_title="Régions au Québec",
+		yaxis_title="Nombre d'évènements",
+
+	)
+	return fig
+	#fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	#total = df.value_counts()
+	#df=df.groupby(['groupe'])['est_gratuit'].count()
+	#print(groupe_by_temp)
+	#print(groupe_by_est_gratuit)
+
+	#temp_df=clus_est_gratuit_data.loc[['Centre'][0]]
+	# print("clus_est_region",clus_est_region)
+	# print("clus_est_gratuit",clus_est_gratuit)
+
+	# print("data is \n",clus_est_gratuit_data)
+	# print("\n #####type of data",type(clus_est_gratuit_data))
+
+
+	# values = list()
+	#print(x_df)
+	# temp_df=clus_est_gratuit_data.to_frame()
+	# print("#####toframe is",temp_df)
+	# print("\ntype of toframe is",type(temp_df))
+	# print("colomn of toframe is",temp_df.columns)
+
+	# for row in clus_est_gratuit:
+	# 	values.append(row)
+
+
+	#print(values)
+	#test=[values[i]  for i in range(len(values))]
+	#print("this is test",test)
+	# true_false_centre=values[0]+values[1]
+	# true_false_montreal=values[2]+values[3]
+	# true_false_nord=values[4]+values[5]
+	# true_false_sud=values[5]+values[6]
+	#
+	# print(x_df[0],true_false_centre)
+	# print(x_df[1],true_false_montreal)
+	# print(x_df[2],true_false_nord)
+	# print(x_df[3],true_false_sud)
+	#print(values)
+	#temp=clus_est_gratuit_data[clus_est_gratuit_data['groupe']=='Centre']
+	#print("temp",temp)
+
+	# temp=clus_est_gratuit_data.loc[('Centre', True)]
+	#fig = px.bar(clus_est_gratuit_data, x="groupe", y="est_gratuit",
+	# color="est_gratuit", title="Long-Form Input"
+	#)
+	#fig.show()
+
+	#print("This is clus_est_gratuit_data\n",clus_est_gratuit_data.loc['groupe'])
+
+
+	# fig = px.bar(df, x="groupe", y="count",
+	#             # color="medal",
+	#              title="THis is a test")
---
+++ code/src/heatmap.py
@@ -0,0 +1,32 @@
+from plotly_calplot import calplot
+from datetime import datetime
+import numpy as np
+from pandas import read_excel, read_csv
+import pandas as pd
+from collections import Counter
+
+
+def get_dates_count(dates:list):
+  count = Counter(dates)
+  dates = list(count.keys())
+  counts = list(count.values())
+  return pd.DataFrame(list(zip(dates, counts)),
+               columns =['date', 'count']).sort_values(by='date',ignore_index=True)
+
+
+def filter_years(df, years:list):
+  data_years = df[df.year.isin(years)].date_debut.to_list()
+  return [pd.to_datetime(d) for d in data_years]
+
+
+def make_heatmap(df, years):
+    data_years = filter_years(df, years)
+    df_day_count = get_dates_count(data_years)
+    fig = calplot(
+        df_day_count,
+        x="date",
+        y="count",
+        name="Number of events",
+        years_title=True,
+    )
+    return fig
